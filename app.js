d33a229d466f2d15c852de1081ecc235f44d6c57
huawin03 <guoqiangqiang@corp.pandatv.com>
2018-03-20 21:15:18 +0800

    route

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ app.use(logger())
 app.use(require('koa-static')(__dirname + '/public'))
 
 app.use(views(__dirname + '/views', {
-  extension: 'pug'
+  // extension: 'pug'
+  map : {html:'ejs'}
 }))
 
 // logger
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
-const router = require('koa-router')()
+const router = require('koa-router')();
 
-router.prefix('/users')
-
+// router.prefix('/users')
+const userModel = require('../lib/index.js');
+const md5 = require('md5');
 router.get('/', function (ctx, next) {
   ctx.body = 'this is a users response!'
 })
